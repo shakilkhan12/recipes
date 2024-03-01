27a16dc5d91959c2ad5bfddd745d79f267b04ed6
shakilkhan12 <shakilkhan621@gmail.com>
2024-03-01 23:22:36 +0500

    changes made

--- src/resolvers/Ingredient.js
+++ src/resolvers/Ingredient.js
@@ -1,6 +1,6 @@
 module.exports = {
   Ingredient: {
-    // just a comment added here comment updated again updated
+    // just a comment added here comment updated again updated third change
     detailedDescription: ({ text }) => text,
   },
 };
